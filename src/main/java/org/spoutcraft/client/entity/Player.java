70563b3a00ede8bb2d2306d6c953043c127a2082
Chris Sanders <zidane@outlook.com>
2013-12-23 19:40:53 -0600

    I forgot that JoinGame sets the local player's ID.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spoutcraft/client/entity/Player.java
+++ src/main/java/org/spoutcraft/client/entity/Player.java
@@ -12,14 +12,10 @@ import org.spout.math.vector.Vector3f;
  * The local client player which has the {@link com.flowpowered.networking.session.Session} tied to it.
  */
 public class Player extends Entity {
-    /**
-     * -1 is reserved for the client player because no other entities can be spawned on the client without a valid id (>= 0).
-     */
-    public final static int LOCALPLAYERID = -1;
     private final ClientSession session;
 
-    public Player(String displayName, World world, Vector3f position, ClientSession session) {
-        super(LOCALPLAYERID, displayName, world, position);
+    public Player(int id, String displayName, World world, Vector3f position, ClientSession session) {
+        super(id, displayName, world, position);
         this.session = session;
     }
 
