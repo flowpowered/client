f7052930dfe06dcca401041dbc3ef8c432fc992c
Chris Sanders <zidane@outlook.com>
2013-12-31 18:17:58 -0600

    Fix memory leak caused by reference to Futures.
    Thanks to @unknownloner for pointing this out
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spoutcraft/client/nterface/mesh/ParallelChunkMesher.java
+++ src/main/java/org/spoutcraft/client/nterface/mesh/ParallelChunkMesher.java
@@ -1,3 +1,26 @@
+/**
+ * This file is part of Client, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2013 Spoutcraft <http://spoutcraft.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.spoutcraft.client.nterface.mesh;
 
 import java.util.concurrent.Callable;
@@ -69,7 +92,7 @@ public class ParallelChunkMesher {
      * model.
      */
     public static class ChunkModel extends Model {
-        private final Future<VertexData> mesh;
+        private Future<VertexData> mesh;
         private ChunkModel previous;
 
         private ChunkModel(Future<VertexData> mesh) {
@@ -87,6 +110,7 @@ public class ParallelChunkMesher {
                 } catch (Exception ex) {
                     throw new RuntimeException(ex);
                 }
+                mesh = null;
                 // Create the vertex array from the mesh
                 final VertexArray vertexArray = Renderer.getGLFactory().createVertexArray();
                 vertexArray.setData(vertexData);
