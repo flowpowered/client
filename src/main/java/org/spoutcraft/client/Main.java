fc3be795a76fbff49e75538a52db90b9d999df5f
Aleksi Sapon <alek42008@yahoo.com>
2014-04-12 21:42:17 -0400

    Move native deployment method to Caustic

--- src/main/java/org/spoutcraft/client/Main.java
+++ src/main/java/org/spoutcraft/client/Main.java
@@ -26,7 +26,7 @@ package org.spoutcraft.client;
 import java.io.File;
 import java.nio.file.Files;
 
-import org.apache.commons.lang3.SystemUtils;
+import org.spout.renderer.lwjgl.LWJGLUtil;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -39,38 +39,6 @@ public class Main {
         if (!configFile.exists()) {
             Files.copy(Main.class.getResourceAsStream("/config.yml"), configFile.toPath());
         }
-        final String osPath;
-        final String[] nativeLibs;
-        if (SystemUtils.IS_OS_WINDOWS) {
-            nativeLibs = new String[]{
-                    "jinput-dx8_64.dll", "jinput-dx8.dll", "jinput-raw_64.dll", "jinput-raw.dll",
-                    "jinput-wintab.dll", "lwjgl.dll", "lwjgl64.dll", "OpenAL32.dll", "OpenAL64.dll"
-            };
-            osPath = "windows/";
-        } else if (SystemUtils.IS_OS_MAC) {
-            nativeLibs = new String[]{
-                    "libjinput-osx.jnilib", "liblwjgl.jnilib", "openal.dylib"
-            };
-            osPath = "mac/";
-        } else if (SystemUtils.IS_OS_LINUX) {
-            nativeLibs = new String[]{
-                    "liblwjgl.so", "liblwjgl64.so", "libopenal.so", "libopenal64.so", "libjinput-linux.so",
-                    "libjinput-linux64.so"
-            };
-            osPath = "linux/";
-        } else {
-            throw new IllegalStateException("Could not get lwjgl natives for OS \"" + SystemUtils.OS_NAME + "\".");
-        }
-        final File nativesDir = new File("natives" + File.separator + osPath);
-        nativesDir.mkdirs();
-        for (String nativeLib : nativeLibs) {
-            final File nativeFile = new File(nativesDir, nativeLib);
-            if (!nativeFile.exists()) {
-                Files.copy(Main.class.getResourceAsStream("/" + nativeLib), nativeFile.toPath());
-            }
-        }
-        final String nativesPath = nativesDir.getAbsolutePath();
-        System.setProperty("org.lwjgl.librarypath", nativesPath);
-        System.setProperty("net.java.games.input.librarypath", nativesPath);
+        LWJGLUtil.deployNatives(null);
     }
 }
